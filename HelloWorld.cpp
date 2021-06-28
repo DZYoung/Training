e67d8a80f5e3bf4c12ca9cd7d54058ad5a582818
戴金成 <1035661785@qq.com>
2021-06-28 19:38:24 +0800

    第一次提交

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+    QLabel label("HelloWorld!");
+    label.setAlignment(Qt::AlignCenter);
+    label.resize(400, 300);
+    label.show();
+    return app.exec();
+}
